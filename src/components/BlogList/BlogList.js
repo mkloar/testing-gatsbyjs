a5a0aa168483d433521dcac4adfbcf7b10923661
mkloar <miha.kloar@gmail.com>
2020-03-07 15:10:31 +0100

    Change how default site looks

---
+++ src/components/BlogList/BlogList.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Blog } from '../../theme'
+import { Link } from 'gatsby'
+
+
+const BlogList = ({ blogs, prefetch }) => {
+    return (
+        <section>
+            {blogs.allMarkdownRemark.edges.map(({ node }) =>  {
+                if(prefetch) {
+                    return (
+                        <Link to={`/blog/${node.frontmatter.path}`} key={node.id}>
+                            <Blog>                        
+                                <h3>{ node.frontmatter.title }</h3>
+                            </Blog>   
+                        </Link>
+                    )
+                }
+                return (
+                    <a href={`/blog/${node.frontmatter.path}`} key={node.id}>
+                        <Blog>                        
+                            <h3>{ node.frontmatter.title }</h3>
+                        </Blog>   
+                    </a>
+                )
+                           
+            })}
+        </section>
+    )
+}
+
+BlogList.defaultProps = {
+    prefetch: true
+}
+
+export default BlogList
--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import { Template } from '../theme'
+import { Link } from 'gatsby'
+import { Template, BackButton } from '../theme'
 
 const BlogTemplate = ({ pageContext: { node } }) => (
     <Template>
+        <BackButton>
+            <Link to="/">Nazaj</Link>
+        </BackButton>
         <h1>{ node.frontmatter.title }</h1>
         <section dangerouslySetInnerHTML={{ __html: node.html }} />
     </Template>
